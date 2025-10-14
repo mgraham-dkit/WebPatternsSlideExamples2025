c6e916506d94772ca4fa38b51ff21b5ab02362d5
michelle <michelle.graham@dkit.ie>
2025-10-14 02:20:09 +0100

    fix: remove unnecessary methods from connector

--- src/main/java/databases/D_introducingLombok/persistence/Connector.java
+++ src/main/java/databases/D_introducingLombok/persistence/Connector.java
@@ -3,7 +3,6 @@ package databases.D_introducingLombok.persistence;
 import java.sql.Connection;
 
 public interface Connector {
-    public void setConnection(Connection conn);
     public Connection getConnection();
     public void freeConnection();
 }
